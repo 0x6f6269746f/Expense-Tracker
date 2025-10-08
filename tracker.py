efb058cedd9aa81bc634cf01192d011c71be0e8c
Tommy Nguyen <tommypnguyen26@gmail.com>
2025-10-08 15:14:46 -0500

    Added Logic

--- tracker.py
+++ tracker.py
@@ -1,7 +1,23 @@
 def main():
     print(f"Running Expense Tracker")
-    
-    pass
+    #Get user expense
+    get_expense()
+    #write expense to file
+    write_expenes()
+    #read the expense
+    read_expense()
 
-if __name__ == "__name__":
+def get_expense():
+    expense = input(f"Enter in an expense: ")
+    print(f"Expense is: {expense}")
+
+def write_expenes():
+    print(f"Write")
+
+
+def read_expense():
+    print(f"Read")
+
+
+if __name__ == "__main__":
     main()
